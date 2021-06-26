60fba57f72eccf61445afe30978229d4767c8edc
ugarci <ugarci@gmail.com>
2021-06-26 10:38:23 -0400

    comment: fom windows cmd

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {                             //ugg + new edit online
 
-    public static void main (String [] args) {
+    public static void main (String [] args) {        // Updates via windows cmd
         System.out.println("Hello world");
     }
 
