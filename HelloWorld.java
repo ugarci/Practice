c4dd8ccd91b9060eb20541baa5967893790cc477
ugarci <ugarci@gmail.com>
2021-06-27 13:43:54 -0400

    anew

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-public class HelloWorld {                             //from newFeature
+public class HelloWorld {                             //from main
 
     public static void main (String [] args) {
         System.out.println("Hello world");
